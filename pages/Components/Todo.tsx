40c9dc58f3c6f0ebb59ec4b7974c4c9481cda848
Setu Potnis <46113818+setupotnis@users.noreply.github.com>
2021-10-31 18:22:07 -0400

    add todo component and its styling

---
+++ pages/Components/Todo.tsx
@@ -0,0 +1,62 @@
+import { NextComponentType } from "next";
+import React, { useState } from "react";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddTask from "./AddTask";
+import styles from "../../styles/Todo.module.css";
+import { IconButton } from "@mui/material";
+
+const Todo: NextComponentType = ({
+  todos,
+  completeTodo,
+  deleteTodo,
+  updateTodo,
+}) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
+
+  const submitUpdate = (value: any) => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: "",
+    });
+  };
+
+  if (edit.id) {
+    return <AddTask edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return todos.map((todo: any, index: number) => (
+    <div
+      className={
+        todo.isComplete
+          ? `${styles.todoRow} + " " + ${styles.complete}`
+          : styles.todoRow
+      }
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className={styles.icons}>
+        <IconButton aria-label="edit">
+          <EditIcon
+            className={styles.editIcon}
+            onClick={() => setEdit({ id: todo.id, text: todo.text })}
+          />
+        </IconButton>
+        <IconButton aria-label="delete">
+          <DeleteIcon
+            className={styles.deleteIcon}
+            onClick={() => deleteTodo(todo.id)}
+          />
+        </IconButton>
+      </div>
+    </div>
+  ));
+};
+
+export default Todo;
